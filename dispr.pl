f177d263ef413931361019925c61c21f15cee159
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-04-09 11:19:07 +0200

    use count() method

--- dispr.pl
+++ dispr.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/env perl
 
+# TODO: multiple primers
+# TODO: remove duplicate hits
+# TODO: allow for settable number of mismatches... how?
+# TODO: fuzzy matching?
+# TODO: identify intervals
+
 use strict;
 use warnings;
 
@@ -16,8 +22,8 @@ my @o_pf = qw/CTNCAYVARCCYATGTAYYWYTTBYT/;
 my @o_pr = qw/GTYYTNACHCYRTAVAYRATRGGRTT/;
 my $o_orientation = "FR";
 my $o_multiplex = 1;
-my $o_min = 300;
-my $o_max = 1000;
+my $o_min = 1;
+my $o_max = 4000;
 my $o_bed = "out.bed";
 my $o_seq = "out.fa";
 my $o_ref = "test.fa";
@@ -42,8 +48,8 @@ of each primer sequence, so includes the primer sequence length.
     --no-multiplex        If more than one primer pair presented, only consider
                           amplicons produced by each primer pair
     --orientation FR      Orientation of primers, only FR supported for now
-    --min bp              Minimum accepted size of amplicon
-    --max bp              Maximum accepted size of amplicon
+    --min bp              Minimum accepted size of amplicon [default $o_min]
+    --max bp              Maximum accepted size of amplicon [default $o_max]
 
     --both                Orientation of the reference sequence
     --forward
@@ -103,12 +109,8 @@ print STDERR "pat_prf: $pat_prf   pat_prr: $pat_prr\n";
 
 my $stream_pf = Bio::Tools::IUPAC->new(-seq => $seq_pf);
 my $stream_pr = Bio::Tools::IUPAC->new(-seq => $seq_pr);
-my $fnumber = 0;
-++$fnumber while $stream_pf->next_seq();
-print STDERR "f: $fnumber expanded sequences\n";
-my $rnumber = 0;
-++$rnumber while $stream_pr->next_seq();
-print STDERR "r: $rnumber expanded sequences\n";
+print STDERR "f: ".$stream_pf->count()." expanded sequences\n";
+print STDERR "f: ".$stream_pr->count()." expanded sequences\n";
 
 
 my $in = Bio::SeqIO->new(-file => "<$o_ref", -format => 'fasta');
